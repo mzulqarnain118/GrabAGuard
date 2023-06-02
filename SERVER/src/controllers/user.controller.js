fc996b327c3e48b9259dc4854bdaafb38d31e48f
mzulqarnain118 <ranamzulqarnain1@gmail.com>
2023-06-02 11:23:26 +0500

    done

--- SERVER/src/controllers/user.controller.js
+++ SERVER/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService,emailService } = require('../services');
+const he = require('he');//for html entities
 
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
@@ -41,12 +42,10 @@ const updateUser = catchAsync(async (req, res) => {
 });
 
 const ApproveUpdateUser = catchAsync(async (req, res) => {
-  const emailSentData = JSON.stringify(req.body)
-  const { updateEmail,updatePhone,updateAbout } = req.body;
   const sentEmail = await emailService.sendEmailWithSES(
-    'ranamzulqarnain1@gmail.com',
+    false,
     'User Data Update Request',
-    "<h1>Update Request</h1><br/>"+req.body.emailString,
+   he.decode(req.body.emailString)
   );
   console.log("sentEmail", sentEmail)
   delete req.body.emailString;
